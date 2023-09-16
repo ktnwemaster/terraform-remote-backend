489f161bf587b1612d2b530ce0f2c80e0835c3d5
ktnwemaster <ktnwemaster@gmail.com>
2023-09-16 01:20:20 +0000

    Updated versions.tf error correction

--- versions.tf
+++ versions.tf
@@ -10,6 +10,6 @@ terraform {
 provider "aws" {
   shared_config_files      = ["/home/vagrant/.aws/config"]
   shared_credentials_files = ["/home/vagrant/.aws/credentials"]
-  profile                  = "var.aws_profile"
+  profile                  = var.aws_profile
   region = var.aws_region
 }
